13f8109af1b40d43efae56a356c9bae0b185d812
64envy64 <korn33v0@mail.ru>
2024-03-10 15:31:01 +0500

    12332122

--- assets/campus.js
+++ assets/campus.js
@@ -1,109 +1,53 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
 
-var tl = gsap.timeline({scrollTrigger:{
-    trigger: ".part-1",
-    start:"50% 50%",
-    end:"250% 50%",
-    scrub:true,
-    // markers:true,
-    pin:true,
-}})
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 6000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
 
-tl.to(".rotate-div",{
-    rotate: -15,
-    scale: 0.8,
-},'a')
-tl.to("#row-div-2",{
-    marginTop: "5%"
-},'a')
-tl.to("#row-div-3",{
-    marginTop: "-2%"
-},'a')
-tl.to("#row-div-4",{
-    marginTop: "-8%"
-},'a')
-tl.to("#row-div-5",{
-    marginTop: "-10%"
-},'a')
-tl.to(".overlay-div h1",{
-    opacity: "1",
-    delay: 0.2,
-},'a')
-tl.to(".overlay-div",{
-    backgroundColor: "#000000b4",
-},'a')
-tl.to(".scrolling",{
-    width: "100%",
-},'a')
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
 
-var tl2 = gsap.timeline({scrollTrigger:{
-    trigger: ".part-2",
-    start:"0% 70%",
-    end:"50% 50%",
-    scrub:true,
-    // markers:true,
-}})
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
 
-tl2.to(".rounded-div-wrapper",{
-    height:0,
-    marginTop: 0
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
 })
-
-
-let tl3 = gsap.timeline({
-    scrollTrigger:{
-        trigger: ".content-2",
-        start:"20% 50%",
-        end: "100% 50%",
-        // markers: true,
-        scrub: 1,
-    },  
-});
-tl3.to(".text-area-hover h1",{
-    width:"100%",
-})
-tl3.to(".text-area-hover h2",{
-    delay: -0.4,
-    width:"100%",
-})
-
-
-let tl4 = gsap.timeline({
-    scrollTrigger:{
-        trigger: ".part-4",
-        start:"25% 25%",
-        end: "200% 50%",
-        pin: true,
-        // markers: true,
-        scrub: 1,
-    },  
-});
-tl4.to(".c-one",{
-    marginTop: "-25%",
-    opacity:"1",
-}, 'sct-1')
-tl4.to(".c-two",{
-    opacity:"1",
-}, 'sct-2')
-tl4.to(".c-one",{
-    marginTop: "-100",
-    opacity:"0",
-}, 'sct-2')
-tl4.to(".c-three",{
-    opacity:"1",
-}, 'sct-3')
-tl4.to(".c-two",{
-    opacity:"0",
-}, 'sct-3')
-tl4.to(".c-one",{
-    marginTop:"-180%",
-}, 'sct-3')
-tl4.to(".c-one",{
-    marginTop:"-230%",
-}, 'sct-4')
-tl4.to(".c-three",{
-    opacity:"0",
-}, 'sct-4')
-tl4.to(".cir-part-4",{
-    marginLeft:"100%",
-    rotate: 360
-}, 'sct-4')
